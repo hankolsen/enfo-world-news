b6206e4f3ba4cce94d004cccf284dbf9cbdff289
hank <henrik@hankolsen.com>
2017-11-29 13:07:18 +0100

    Added service worker and loading item

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* global self caches:true */
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('enfo-wordl-news')
+      .then(cache =>
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/css/normalize.css',
+          '/css/main.css',
+        ]),
+      ),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log(event.request.url);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request)),
+  );
+});
